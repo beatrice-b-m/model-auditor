eceace4c6ea67046edf94634aa71ba99079f02ee
beatrice <bbrownmulry@gmail.com>
2025-04-09 18:48:28 -0400

    initialize wip evaluate_score function to evaluate a single score column

--- model_auditor/core.py
+++ model_auditor/core.py
@@ -1,8 +1,12 @@
-from typing import Optional, Union, Callable
-from model_auditor.schemas import AuditorFeature, AuditorScore, AuditorOutcome
+from typing import Optional, Type, Union, Callable
 import pandas as pd
 import numpy as np
 from sklearn.metrics import roc_curve
+
+from model_auditor.metric_inputs import AuditorMetricInput
+from model_auditor.metrics import AuditorMetric
+from model_auditor.schemas import AuditorFeature, AuditorScore, AuditorOutcome
+from model_auditor.utils import collect_input_metrics
 
 
 class Auditor:
@@ -32,6 +36,10 @@ class Auditor:
         if outcome is not None:
             self.add_outcome(**vars(outcome))
 
+        # initialize attrs for later
+        self._inputs: list[Type[AuditorMetricInput]] = list()
+        self._evaluations: list = list()
+
     def add_data(self, data: pd.DataFrame) -> None:
         self.data = data.copy()
 
@@ -56,13 +64,13 @@ class Auditor:
         self.scores[score.name] = score
 
     def add_outcome(self, name: str, mapping: Optional[dict[any, int]] = None) -> None:
-        if self.data == None:
+        if self.data is None:
             raise ValueError("Please add data with .add_data() first")
 
         if mapping is not None:
-            self.data["_outcome"] = self.data[name].map(mapping)
+            self.data["_truth"] = self.data[name].map(mapping)
         else:
-            self.data["_outcome"] = self.data[name]
+            self.data["_truth"] = self.data[name]
 
     def optimize_score_threshold(self, score_name: str) -> float:
         if len(self.scores) == 0:
@@ -81,6 +89,7 @@ class Auditor:
         # otherwise the target score will be the single item in the list
         truth_list: list[float] = self.data["_outcome"].astype(float).tolist()
 
+        # calculate optimal threshold
         fpr, tpr, thresholds = roc_curve(truth_list, score_list)
         idx: int = np.argmax(tpr - fpr).astype(int)
         optimal_threshold: float = thresholds[idx]
@@ -89,3 +98,59 @@ class Auditor:
             f"Optimal threshold for '{score.label}' [{score.name}] found at: {optimal_threshold}"
         )
         return optimal_threshold
+
+    def define_metrics(self, metrics: list[AuditorMetric]) -> None:
+        self.metrics: list[AuditorMetric] = metrics
+
+    def collect_inputs(self) -> None:
+        inputs_set: set[str] = set()
+        for metric in self.metrics:
+            inputs_set.update(metric.inputs)
+
+        inputs_dict: dict[str, Type[AuditorMetricInput]] = collect_input_metrics()
+
+        # reinit self._inputs and add all necessary inputs to it
+        self._inputs: list[Type[AuditorMetricInput]] = list()
+        for input_name in list(inputs_set):
+            self._inputs.append(inputs_dict[input_name])
+
+    def evaluate_score(
+        self, score_name: str, threshold: Optional[float] = None
+    ) -> None:
+        if self.data is None:
+            raise ValueError("Please add data with .add_data() first")
+
+        # get score
+        score: AuditorScore = self.scores[score_name]
+
+        if (threshold is None) & (score.threshold is None):
+            raise ValueError(
+                "Threshold must be defined in score object or passed to .evaluate_score()"
+            )
+        elif threshold is None:
+            threshold = score.threshold
+
+        # copy a slice of the dataframe with the score col and the outcome column
+        data_slice: pd.DataFrame = self.data.loc[:, ["_truth", score.name]]
+        for input_type in self._inputs:
+            metric_input = input_type()
+            data_slice: pd.DataFrame = metric_input.data_transform(data_slice)
+
+        for feature in self.features.values():
+            # cast feature levels to string
+            data_slice[feature.name] = data_slice[feature.name].astype(str)
+
+            # then group the df by the feature and get all metrics for each
+            feature_groups = data_slice.groupby(feature.name)
+
+            # e.g.
+            feature_metrics: dict[str, dict[str, Union[float, int]]] = dict()
+            for metric in self.metrics:
+                # e.g. {'levelA': 0.2, 'levelB': 0.4}
+                feature_level_metrics: dict[str, Union[float, int]] = (
+                    feature_groups.apply(metric.data_call).to_dict()
+                )
+
+                feature_metrics[metric.name] = feature_level_metrics
+
+            # RESUME HERE !
