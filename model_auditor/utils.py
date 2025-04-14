8f6a0c9a3bb97073157e8b0393fe70af22f34592
Beatrice Brown-Mulry <beatrice.bm@emory.edu>
2025-04-14 18:31:41 -0400

    modify dynamic metric input collection function to enforce protocol endpoints rather than subclass

--- model_auditor/utils.py
+++ model_auditor/utils.py
@@ -4,13 +4,24 @@ from typing import Type
 from model_auditor.metric_inputs import AuditorMetricInput
 
 
-def collect_input_metrics() -> dict[str, Type[AuditorMetricInput]]:
+def is_metric_input_valid(cls: type) -> bool:
+    return (
+        inspect.isclass(cls)
+        and hasattr(cls, "name")
+        and hasattr(cls, "label")
+        and hasattr(cls, "inputs")
+        and callable(getattr(cls, "row_call", None))
+        and callable(getattr(cls, "data_transform", None))
+    )
+
+
+def collect_metric_inputs() -> dict[str, Type[AuditorMetricInput]]:
     module = importlib.import_module("model_auditor.metric_inputs")
 
     input_classes = {
         cls.name: cls
         for _, cls in inspect.getmembers(module, inspect.isclass)
-        if issubclass(cls, AuditorMetricInput) and cls is not AuditorMetricInput
+        if is_metric_input_valid(cls) and cls is not AuditorMetricInput
     }
 
     return input_classes
