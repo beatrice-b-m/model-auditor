071b4208a1df6400e11913a790449879585041a4
Beatrice Brown-Mulry <beatrice.bm@emory.edu>
2025-04-14 18:32:55 -0400

    introduction of eval schemas + general schema updates + to_dataframe methods for eval schemas

--- model_auditor/schemas.py
+++ model_auditor/schemas.py
@@ -1,5 +1,137 @@
-from typing import Optional
-from dataclasses import dataclass
+from typing import Optional, Union
+from dataclasses import dataclass, field
+
+import pandas as pd
+
+
+@dataclass
+class LevelMetric:
+    """
+    Object to store the evaluation results for one metric of one level of a feature.
+    (for example, AUC for one category of finding)
+
+    Args:
+        name (str): Name of the current feature level metric
+        score (float): Score for the current feature level metric
+        interval (tuple[float, float], optional): Optional lower and upper confidence
+        bounds for the current feature level metric (defaults to None)
+    """
+
+    name: str
+    label: str
+    score: float
+    interval: Optional[tuple[float, float]] = None
+
+
+@dataclass
+class LevelEvaluation:
+    """
+    Object to store the evaluation results for one level of a feature
+    (for example, all metrics for one category of finding).
+
+    Args:
+        name (str): Name of the current feature level
+        metrics (dict[str, LevelMetric]): Metrics for the current feature level
+        (defaults to an empty dict)
+    """
+
+    name: str
+    metrics: dict[str, LevelMetric] = field(default_factory=dict)
+
+    def update(self, metric_name: str, metric_label: str, metric_score: float) -> None:
+        self.metrics[metric_name] = LevelMetric(
+            name=metric_name, label=metric_label, score=metric_score
+        )
+
+    def update_intervals(self, metric_intervals: dict[str, tuple[float, float]]):
+        for metric_name, confidence_interval in metric_intervals.items():
+            self.metrics[metric_name].interval = confidence_interval
+
+    def to_dataframe(self, n_decimals: int = 3, add_index: bool = False):
+        metric_data: dict[str, str] = dict()
+        for metric in self.metrics.values():
+            if metric.interval is not None:
+                metric_data[metric.name] = (
+                    f"{metric.score:.{n_decimals}f} ({metric.interval[0]:.{n_decimals}f}, {metric.interval[1]:.{n_decimals}f})"
+                )
+            else:
+                metric_data[metric.name] = f"{metric.score:.{n_decimals}f}"
+
+        return pd.DataFrame(metric_data, index=[self.name])
+
+
+@dataclass
+class FeatureEvaluation:
+    """
+    Object to store the evaluation results for one feature type
+    (for example, metrics associated with different types of findings)
+
+    Args:
+        name (str): Name of the current feature
+        name (str): Label for the current feature
+        levels (dict[str, LevelEvaluation]): Levels of the current feature
+        (defaults to an empty dict)
+    """
+
+    name: str
+    label: str
+    levels: dict[str, LevelEvaluation] = field(default_factory=dict)
+
+    def update(
+        self, metric_name: str, metric_label: str, data: dict[str, float]
+    ) -> None:
+        # expects a dict for one metric type: {'levelA': 0.5, 'levelB': 0.5}
+        # and maps them to child level metric dicts
+        for level_name, level_metric in data.items():
+            # try to get the level item and instantiate a new one if it doesn't exist yet
+            level_eval: LevelEvaluation = self.levels.get(
+                level_name, LevelEvaluation(name=level_name)
+            )
+            # update the metrics for that level eval object and save it back to the dict
+            level_eval.update(
+                metric_name=metric_name,
+                metric_label=metric_label,
+                metric_score=level_metric,
+            )
+            self.levels[level_name] = level_eval
+
+    def update_intervals(
+        self, level_name: str, metric_intervals: dict[str, tuple[float, float]]
+    ):
+        self.levels[level_name].update_intervals(metric_intervals=metric_intervals)
+
+    def to_dataframe(
+        self, n_decimals: int = 3, add_index: bool = False
+    ) -> pd.DataFrame:
+        data: list[pd.DataFrame] = []
+        for level_data in self.levels.values():
+            data.append(level_data.to_dataframe(n_decimals=n_decimals))
+
+        if add_index:
+            return pd.concat({self.label: pd.concat(data, axis=0)})
+        else:
+            return pd.concat(data, axis=0)
+
+
+@dataclass
+class ScoreEvaluation:
+    name: str
+    label: str
+    features: dict[str, FeatureEvaluation] = field(default_factory=dict)
+
+    def to_dataframe(
+        self, n_decimals: int = 3, add_index: bool = False
+    ) -> pd.DataFrame:
+        data: list[pd.DataFrame] = []
+        for feature_data in self.features.values():
+            data.append(
+                feature_data.to_dataframe(n_decimals=n_decimals, add_index=True)
+            )
+
+        if add_index:
+            return pd.concat({self.label: pd.concat(data, axis=0)})
+        else:
+            return pd.concat(data, axis=0)
 
 
 @dataclass
